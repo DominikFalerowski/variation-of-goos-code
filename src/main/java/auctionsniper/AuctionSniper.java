4614d05bfc50e3a363fb2ff4d15b2dec164f670f
dominikf <b3leek@gmail.com>
2020-09-08 15:04:29 +0200

    Make passing report lose when auction close.

--- src/main/java/auctionsniper/AuctionSniper.java
+++ src/main/java/auctionsniper/AuctionSniper.java
@@ -1,11 +1,19 @@
 package auctionsniper;
 
-public class AuctionSniper {
+public class AuctionSniper implements AuctionEventListener {
+
+    private final SniperListener sniperListener;
 
     public AuctionSniper(SniperListener sniperListener) {
+        this.sniperListener = sniperListener;
     }
 
     public void auctionClosed() {
+        sniperListener.sniperLost();
+    }
 
+    @Override
+    public void currentPrice(int price, int increment) {
+        // TODO implement later
     }
 }
