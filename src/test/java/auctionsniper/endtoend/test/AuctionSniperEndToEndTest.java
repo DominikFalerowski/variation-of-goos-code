d4c4f7c4906d577e99a60bbeb5cc07264edf02b9
dominikf <b3leek@gmail.com>
2020-09-10 18:01:32 +0200

    Add method parameter to attend more than 1 auction.

--- src/test/java/auctionsniper/endtoend/test/AuctionSniperEndToEndTest.java
+++ src/test/java/auctionsniper/endtoend/test/AuctionSniperEndToEndTest.java
@@ -8,11 +8,13 @@ import org.jxmpp.stringprep.XmppStringprepException;
 class AuctionSniperEndToEndTest {
 
     private FakeAuctionServer auction;
+    private FakeAuctionServer auction2;
     private ApplicationRunner application;
 
     @BeforeEach
     void setUp() throws XmppStringprepException {
         auction = new FakeAuctionServer("item-54321");
+        auction2 = new FakeAuctionServer("item-65432");
         application = new ApplicationRunner();
     }
 
@@ -37,7 +39,7 @@ class AuctionSniperEndToEndTest {
         application.startBiddingIn(auction);
         auction.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
         auction.reportPrice(1000, 98, "other bidder");
-        application.hasShownSniperIsBidding(1000, 1098);
+        application.hasShownSniperIsBidding(auction, 1000, 1098);
         auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
         auction.announceClosed();
         application.showSniperHasLostAuction(1000, 1098);
@@ -49,7 +51,7 @@ class AuctionSniperEndToEndTest {
         application.startBiddingIn(auction);
         auction.hasReceivedJoinRequestFromSniper(ApplicationRunner.SNIPER_XMPP_ID);
         auction.reportPrice(1000, 98, "other bidder");
-        application.hasShownSniperIsBidding(1000, 1098); //last price, last bid
+        application.hasShownSniperIsBidding(auction, 1000, 1098); //last price, last bid
         auction.hasReceivedBid(1098, ApplicationRunner.SNIPER_XMPP_ID);
         auction.reportPrice(1098, 97, ApplicationRunner.SNIPER_XMPP_ID);
         application.hasShownSniperIsWinning(1098); //winning bid
