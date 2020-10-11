694a0dd0321f8636a61687984f97fde03ea17852
dominikf <b3leek@gmail.com>
2020-10-11 16:51:22 +0200

    Created VO Item to store identifier and stopPrice.

---
+++ src/main/java/auctionsniper/Item.java
@@ -0,0 +1,44 @@
+package auctionsniper;
+
+import java.util.Objects;
+
+class Item {
+
+    private final String identifier;
+    private final int stopPrice;
+
+    public Item(String identifier, int stopPrice) {
+        this.identifier = identifier;
+        this.stopPrice = stopPrice;
+    }
+
+    public String getIdentifier() {
+        return identifier;
+    }
+
+    public int getStopPrice() {
+        return stopPrice;
+    }
+
+    @Override
+    public String toString() {
+        return "Item{" +
+                "identifier='" + identifier + '\'' +
+                ", stopPrice=" + stopPrice +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Item item = (Item) o;
+        return stopPrice == item.stopPrice &&
+                Objects.equals(identifier, item.identifier);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(identifier, stopPrice);
+    }
+}
